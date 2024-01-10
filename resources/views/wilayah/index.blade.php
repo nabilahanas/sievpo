ea4f16118e594e8ca441b2aaa419fb239ba4042d
atarikatrimelfin@gmail.com <atarikatrimelfin@gmail.com>
2024-01-11 00:51:44 +0700

    Update index.blade.php

--- resources/views/wilayah/index.blade.php
+++ resources/views/wilayah/index.blade.php
@@ -27,3 +27,30 @@
                     <th>Aksi</th>
                 </tr>
             </thead>
+            <tbody>
+                @foreach($wilayahs as $wilayah)
+                    <tr>
+                        <td>{{ $wilayah->nama_wilayah }}</td>
+                        <td>{{ $wilayah->latitude }}</td>
+                        <td>{{ $wilayah->longitude }}</td>
+                        <td>{{ $wilayah->deskripsi }}</td>
+                        <td>
+                            <!-- Tambahkan tombol-tombol aksi sesuai kebutuhan -->
+                            <button class="btn btn-sm btn-info">Detail</button>
+                            <button class="btn btn-sm btn-warning">Edit</button>
+                            <button class="btn btn-sm btn-danger">Hapus</button>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+    <!-- Bootstrap JavaScript (optional) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-rHyoN1iRsVXV4nD0Jut5XuOVfdIoA01fSkzB7ti7ihFdaLl5+qXaVi0B2A2vcybp"
+        crossorigin="anonymous"></script>
+
+</body>
+</html>
+

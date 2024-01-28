ba092244097ccdc4fea5bf3bb9173d481ec87250
nabilahanas <mizunaraya@gmail.com>
2024-01-28 17:24:36 +0700

    sidebar

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -4,94 +4,17 @@
 
 @section('content')
     <title>Dashboard</title>
-    <div class="card">
-        <div class="card-body">
-            <div class="row">
 
-                <div class="col-xl-3 col-md-6">
-                    <div class="card bg-primary text-white mb-4">
-                        <div class="card-body">Data Eviden</div>
-                        <div class="card-footer d-flex align-items-center justify-content-between">
-                            <a class="small text-white stretched-link" href="/evdata">View Details</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-xl-3 col-md-6">
-                    <div class="card bg-secondary text-white mb-4">
-                        <div class="card-body">Rekap Eviden Poin</div>
-                        <div class="card-footer d-flex align-items-center justify-content-between">
-                            <a class="small text-white stretched-link" href="/rekapdata/rekap">View Details</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-xl-3 col-md-6">
-                    <div class="card bg-warning text-white mb-4">
-                        <div class="card-body">User</div>
-                        <div class="card-footer d-flex align-items-center justify-content-between">
-                            <a class="small text-white stretched-link" href="/user">View Details</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-xl-3 col-md-6">
-                    <div class="card bg-info text-white mb-4">
-                        <div class="card-body">Profil</div>
-                        <div class="card-footer d-flex align-items-center justify-content-between">
-                            <a class="small text-white stretched-link" href="/profile">View Details</a>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-
-            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img class="d-block w-100" src="https://www.perhutani.co.id/wp-content/uploads/2022/03/hero-1.jpg"
-                            alt="First slide">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block w-100"
-                            src="https://cdn-asset.jawapos.com/wp-content/uploads/2022/04/ilustrasi-kegiatan-perhutani-ist.jpeg"
-                            alt="Second slide">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block w-100"
-                            src="https://www.perhutani.co.id/wp-content/uploads/2022/03/Foto-HUT-PHT-2022.jpg"
-                            alt="Third slide">
-                    </div>
-                </div>
-                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
-            </div>
-        </div>
-    </div>
-    </div>
-@endsection
-
-<!-- Dropdown -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous">
-</script>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
-</script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
-    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
-</script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
-    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
-</script>
+    <!-- Dropdown -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous">
+    </script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,127 @@
+<nav class="mt-2">
+    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <li class="nav-item">
+            <a href="{{ url('/home') }}" class="nav-link {{ $key == 'dashboard' ? 'active' : '' }}">
+                <i class="nav-icon fas fa-tachometer-alt"></i>
+                <p>
+                    Dashboard
+                </p>
+            </a>
+        </li>
+        <li
+            class="nav-item {{ $key == 'harian' || $key == 'bulanan' || $key == 'total' ? 'menu-is-opening menu-open' : '' }}">
+            <a href="{{ url('#') }}"
+                class="nav-link {{ $key == 'harian' || $key == 'bulanan' || $key == 'total' ? 'active' : '' }}">
+                <i class="nav-icon fas fa-table"></i>
+                <p>
+                    Rekap Data
+                    <i class="fas fa-angle-left right"></i>
+                </p>
+            </a>
+            <ul class="nav nav-treeview">
+                <li class="nav-item">
+                    <a href="#" class="nav-link {{ $key == 'harian' ? 'active' : '' }}">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Harian</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link {{ $key == 'bulanan' ? 'active' : '' }}">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Bulanan</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link {{ $key == 'total' ? 'active' : '' }}">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Total</p>
+                    </a>
+                </li>
+            </ul>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link {{ $key == 'konfirmasi' ? 'active' : '' }}">
+                <i class="nav-icon fas fa-check-circle"></i>
+                <p>
+                    Konfirmasi Data
+                </p>
+            </a>
+        </li>
+        <li
+            class="nav-item {{ $key == 'jabatan' || $key == 'bidang' || $key == 'wilayah' || $key == 'lokasi' || $key == 'shift' ? 'menu-is-opening menu-open' : '' }}">
+            <a href="{{ url('#') }}"
+                class="nav-link {{ $key == 'jabatan' || $key == 'bidang' || $key == 'wilayah' || $key == 'lokasi' || $key == 'shift' ? 'active' : '' }}">
+                <i class="nav-icon fas fa-database"></i>
+                <p>
+                    Data Master
+                    <i class="fas fa-angle-left right"></i>
+                </p>
+            </a>
+            <ul class="nav nav-treeview">
+                <li class="nav-item">
+                    <a href="#" class="nav-link {{ $key == 'jabatan' ? 'active' : '' }}">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Jabatan</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link {{ $key == 'bidang' ? 'active' : '' }}">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Bidang</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link {{ $key == 'wilayah' ? 'active' : '' }}">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Wilayah</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link {{ $key == 'lokasi' ? 'active' : '' }}">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Lokasi</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link {{ $key == 'shift' ? 'active' : '' }}">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Shift</p>
+                    </a>
+                </li>
+            </ul>
+        </li>
+        <li
+            class="nav-item {{ $key == 'berita' || $key == 'pengumuman' ? 'menu-is-opening menu-open' : '' }}">
+            <a href="{{ url('#') }}"
+                class="nav-link {{ $key == 'berita' || $key == 'pengumuman' ? 'active' : '' }}">
+                <i class="nav-icon fas fa-newspaper"></i>
+                <p>
+                    Manajemen Publikasi
+                    <i class="fas fa-angle-left right"></i>
+                </p>
+            </a>
+            <ul class="nav nav-treeview">
+                <li class="nav-item">
+                    <a href="#" class="nav-link {{ $key == 'berita' ? 'active' : '' }}">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Berita</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link {{ $key == 'pengumuman' ? 'active' : '' }}">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Pengumuman</p>
+                    </a>
+                </li>
+            </ul>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link {{ $key == 'user' ? 'active' : '' }}">
+                <i class="nav-icon fas fa-users"></i>
+                <p>
+                    User
+                </p>
+            </a>
+        </li>
+    </ul>
+</nav>
